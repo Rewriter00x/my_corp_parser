be2fb75f443a3677048d1288710c58f2024b4039
Rewriter00x <colossus00x@icloud.com>
2023-11-15 12:22:52 +0200

    Initial examples from practice

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
-use my_parser_kma_group_3_DB::*;
+use anyhow::anyhow;
+//use my_parser_kma_group_3_DB::*;
+use pest::Parser;
+use pest_derive::Parser;
 
-pub fn main() {
-    println!("{:?}", list_parser::list("[1,1,2,3,5,8]"));
+#[derive(Parser)]
+#[grammar = "./grammar.pest"]
+struct Grammar;
+
+pub fn main() -> anyhow::Result<()> {
+    //println!("{:?}", list_parser::list("[1,1,2,3,5,8]"));
+
+    Ok(())
+}
+
+#[test]
+pub fn basic_test() -> anyhow::Result<()> {
+    let got = Grammar::parse(Rule::field, "131.13")?.next().ok_or_else(|| anyhow!("no pair"))?;
+    dbg!(got);
+
+    Ok(())
 }
